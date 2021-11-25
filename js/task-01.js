1d787dd01ff67576a99c159002361a50477ece0b
Siniavskay-Nadejda <87280720+Siniavskay-Nadejda@users.noreply.github.com>
2021-11-25 22:49:51 +0200

    coхроняет таск 1

---
+++ js/task-01.js
@@ -0,0 +1,41 @@
+const itemRef = document.querySelectorAll(".item");
+console.log("Number of categories:", itemRef.length);
+
+const titleRef = document.querySelectorAll("h2")
+console.log("Category:", titleRef[0].textContent)
+
+const ulRef = document.querySelectorAll("ul")
+
+
+const liRef = ulRef[1].querySelectorAll("li")
+console.log("Elements:", liRef.length)
+
+const titleRefProduct = document.querySelectorAll("h2")
+console.log("Category:", titleRef[1].textContent)
+
+
+const liRefProduct = ulRef[2].querySelectorAll("li")
+console.log("Elements:", liRefProduct.length)
+
+const titleRefTechnologes = document.querySelectorAll("h2")
+console.log("Category:", titleRefTechnologes[2].textContent)
+
+const liRefTechnologes = ulRef[3].querySelectorAll("li")
+console.log("Elements:", liRefTechnologes.length)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
