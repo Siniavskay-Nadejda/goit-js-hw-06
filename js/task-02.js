35b9eab6bb3fce7fef51a68a5b79ca249b109231
Siniavskay-Nadejda <87280720+Siniavskay-Nadejda@users.noreply.github.com>
2021-11-26 10:15:22 +0200

    сохраняет таск 2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,49 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const itemEl1 = document.createElement("li");
+itemEl1.classList.add("item");
+itemEl1.textContent = 'Potatoes';
+
+const ulEl = document.querySelector("#ingredients");
+ulEl.appendChild(itemEl1);
+
+
+const itemEl2 = document.createElement("li");
+itemEl2.classList.add("item");
+itemEl2.textContent = 'Mushrooms';
+ulEl.appendChild(itemEl2);
+
+
+const itemEl3 = document.createElement("li");
+itemEl3.classList.add("item");
+itemEl3.textContent = 'Garlic';
+ulEl.appendChild(itemEl3);
+
+
+const itemEl4 = document.createElement("li");
+itemEl4.classList.add("item");
+itemEl4.textContent = 'Tomatos';
+ulEl.appendChild(itemEl4);
+
+
+const itemEl5 = document.createElement("li");
+itemEl5.classList.add("item");
+itemEl5.textContent = 'Herbs';
+ulEl.appendChild(itemEl5);
+
+
+const itemEl6 = document.createElement("li");
+itemEl6.classList.add("item");
+itemEl6.textContent = 'Condiments';
+ulEl.appendChild(itemEl6);
+
+
+
+
+
+
+
+
+
